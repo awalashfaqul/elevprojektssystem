3c75c9d53bcc8584bccc70626c44696ecaeffee7
Ashfaqul Awal <40667062+awalashfaqul@users.noreply.github.com>
2026-07-22 20:22:28 +0200

    working with get-student-api

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,17 +1,35 @@
 import express, { response } from 'express'
 import cors from 'cors'
+import pool from "./db.js"
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
+/*
 app.get('/', (_request, response) => {
   response.send('Elevprojektssystemets API är igång')
 })
+*/
 
 app.get('/api/health', (_request, response) => {
   response.status(200).json({ status: 'ok' })
 })
 
+app.get('/api/students', async (_request, response) => {
+  try {
+    const result = await pool.query(
+      `SELECT id, first_name, last_name, email, created_at
+       FROM students
+       ORDER BY id`
+    )
+      response.status(200).json(result.rows)
+  } catch (error){
+    console.error('Kunde inte hitta elever.', error)
+    response.status(500).json({
+      message: 'Kunde inte hämta elever.'
+    })
+  }
+})
 export default app
